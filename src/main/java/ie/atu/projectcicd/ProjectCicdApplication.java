0abe9671bb09048e85eeb5ec19c913a2f8ddaeaa
Tamer Zraiq <G00425053@atu.ie>
2024-11-12 11:46:39 +0000

    Initial commit

---
+++ src/main/java/ie/atu/projectcicd/ProjectCicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.projectcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/projectcicd/ProjectCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
